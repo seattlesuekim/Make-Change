845b970d4fc9e56b12da64e2b74634caa6fec874
Sue White <sue@mailworks.org>
2013-10-30 13:20:58 -0700

    I meant to commit this file and not the previous one.

--- change.rb
+++ change.rb
@@ -1,20 +1,23 @@
 def pos
 	puts "Enter the total: "
 	totalF = gets.chomp.to_f
-	puts "Enter the amount given: "
-	givenF = gets.chomp.to_f
-	change = ((givenF - totalF) * 100).to_i
-	while change < 0 do
-		puts "The total was #{totalF}. We need $#{totalF - givenF} more. Fork it over."
+	while true do
+		puts "Enter the amount given: "
+		givenF = gets.chomp.to_f
+		change = ((givenF - totalF) * 100).to_i
 		if change >= 0 then
-			break
+			return change
 		else
-			redo
+			puts "The total was #{totalF}. We need $#{totalF - givenF} more. Fork it over."
+			next
 		end
 	end
 end
 
-def determineChange
+
+def determineChange(pos)
+
+	change = pos()
 	while change > 0 do
 		dollars = (change / 100).floor
 		change -= dollars
@@ -26,9 +29,7 @@ def determineChange
 		change -= nickels
 		cents = change
 	end
-end
-
-def giveChange
 	puts "Give the customer $#{dollars}, #{quarters} quarters, #{dimes} dimes, #{nickels} nickels, and #{change} cents."
 end
 
+determineChange(pos)
